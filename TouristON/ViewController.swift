87cd7d96864465fc64ff969b2996465b541d14fb
Nathan <kemelbekov-adil@mail.ru>
2019-12-01 15:04:03 +0600

    first

---
+++ TouristON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouristON
+//
+//  Created by Nathan on 01.12.2019.
+//  Copyright © 2019 Nathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
